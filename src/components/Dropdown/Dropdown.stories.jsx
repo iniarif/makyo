f8dd12fe220e2a06542ef23e0ae1d27a03dc31bb
Arif Troena <arif.troena@gmail.com>
2025-02-04 04:39:17 +0700

    :zap: add storybook and components dropdown

---
+++ src/components/Dropdown/Dropdown.stories.jsx
@@ -0,0 +1,17 @@
+import Dropdown from "./Dropdown";
+
+export default {
+  title: "Components/Dropdown",
+  component: Dropdown,
+};
+
+const options = [
+  { label: "Option 1", value: "1" },
+  { label: "Option 2", value: "2" },
+  { label: "Option 3", value: "3" },
+];
+
+export const Default = () => <Dropdown options={options} />;
+export const MultipleSelection = () => <Dropdown options={options} multiple />;
+export const Searchable = () => <Dropdown options={options} searchable />;
+export const CustomRender = () => <Dropdown options={options} customRender={(option) => <span className="text-red-500">{option.label}</span>} />;
