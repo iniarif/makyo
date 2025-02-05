4d39e49b45d9fc071dd900c8d97150ab997b2aba
Arif Troena <arif.troena@gmail.com>
2025-02-05 16:48:00 +0700

    :hammer: update default Label

--- src/components/Dropdown/Dropdown.stories.jsx
+++ src/components/Dropdown/Dropdown.stories.jsx
@@ -25,10 +25,10 @@ export default {
 };
 
 export const SelectDropdownField = {
-  args: {
-    id: "sdd-1",
-    withSearch: true,
-    options: [
+  args:{
+    id:"sdd-1",
+    withSearch:true,
+    options:[
       { label: "Option 1", value: "1" },
       { label: "Option with icon", value: "2" },
       { label: "Long Long Option 3", value: "3" },
@@ -36,8 +36,8 @@ export const SelectDropdownField = {
       { label: "Long Long Long Long Option 5", value: "5" },
       { label: "Long Long Long Long Long Option 6", value: "6" },
     ],
-    multiple: false,
-    optionLabel: "arif",
-    outlined: true,
+    multiple:true,
+    optionLabel:"Label",
+    outlined:true,
   },
 };
