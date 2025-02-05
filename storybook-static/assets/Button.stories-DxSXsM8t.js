073eb4abeba2c47e26f3be4a483d1276f4f327eb
Arif Troena <arif.troena@gmail.com>
2025-02-05 16:19:53 +0700

    :zap: deployement to vercel

---
+++ storybook-static/assets/Button.stories-DxSXsM8t.js
@@ -0,0 +1,25 @@
+import{f as k}from"./index-Bn9rm4iU.js";import{B as z}from"./Button-B-8RaShp.js";import"./jsx-runtime-BZK_9YYG.js";import"./index-cH8BGxVM.js";const L={title:"Example/Button",component:z,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:k()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}},s={args:{primary:!1,label:"Button"}};var t,n,c;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var l,m,p;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var u,i,d;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(d=(i=e.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var g,B,y;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(y=(B=o.parameters)==null?void 0:B.docs)==null?void 0:y.source}}};var b,S,f;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: "Button"
+  }
+}`,...(f=(S=s.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};const M=["Primary","Secondary","Large","Small","Makyo"];export{e as Large,s as Makyo,r as Primary,a as Secondary,o as Small,M as __namedExportsOrder,L as default};
---
+++ storybook-static/assets/index-D4rL5Va7.js
@@ -0,0 +1,1 @@
+import{g as r}from"./index-cH8BGxVM.js";import{r as o}from"./index-BoFgTKXk.js";var t=o();const m=r(t);export{m as M,t as r};
